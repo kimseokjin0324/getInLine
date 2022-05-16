308cb3ec2f119de173ff1571bee66a271ba8583d
kimseokjin <kimseokjin0324@gmail.com>
2022-05-17 01:02:33 +0900

    Update #2 - BaseController and BaseErrorController
    베이스 컨트롤러에서 에러 처리 부분을 분리시킴
    에러 내용을 관찰할 수 있게끔 로직 고도화하고, 템플릿 코드 추가

--- src/main/java/com/example/getInLine/controller/BaseController.java
+++ src/main/java/com/example/getInLine/controller/BaseController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BaseController {
 
     @GetMapping("/")
-    public String root() throws Exception {
-        throw new Exception("테스트");
-//        return "index";
+    public String root() {
+        return "index";
     }
 
 
--- src/main/java/com/example/getInLine/error/BaseErrorController.java
+++ src/main/java/com/example/getInLine/error/BaseErrorController.java
@@ -1,8 +1,11 @@
 package com.example.getInLine.error;
 
 import com.example.getInLine.constant.ErrorCode;
+import com.example.getInLine.dto.APIErrorResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,8 +16,8 @@ import java.util.Map;
 @Controller
 public class BaseErrorController implements ErrorController {
 
-    @RequestMapping("/error")
-    public ModelAndView error(HttpServletResponse response) {
+    @RequestMapping(path="/error",produces = MediaType.TEXT_HTML_VALUE) //-TextHTML을 accessHeader로 가지고있는것만
+    public ModelAndView errorHtml(HttpServletResponse response) {
         HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
         ErrorCode errorCode = httpStatus.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
 
@@ -26,5 +29,16 @@ public class BaseErrorController implements ErrorController {
                         "message", errorCode.getMessage(httpStatus.getReasonPhrase())
                 ),
                 httpStatus);
+    }
+
+    @RequestMapping("/error")
+    public ResponseEntity<APIErrorResponse> error(HttpServletResponse response) {
+        HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
+        ErrorCode errorCode = httpStatus.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(APIErrorResponse.of(false, errorCode));
+
     }
 }
