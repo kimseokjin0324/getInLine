6fbeb626d7d5d32aea5ec7fc766fc4114582124c
kimseokjin <kimseokjin0324@gmail.com>
2022-05-19 23:15:10 +0900

    Update #2 PlaceDTO
    merge error fix

--- src/main/java/com/example/getInLine/dto/PlaceDTO.java
+++ src/main/java/com/example/getInLine/dto/PlaceDTO.java
@@ -2,33 +2,24 @@ package com.example.getInLine.dto;
 
 import com.example.getInLine.constant.PlaceType;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
 
-=======
->>>>>>> #2-api
 public record PlaceDTO(
         PlaceType placeType,
         String placeName,
         String address,
         String phoneNumber,
         Integer capacity,
-<<<<<<< HEAD
         String memo,
         LocalDateTime createdAt,
         LocalDateTime modifiedAt
 ) {
-=======
-        String memo){
-
->>>>>>> #2-api
     public static PlaceDTO of(
             PlaceType placeType,
             String placeName,
             String address,
             String phoneNumber,
             Integer capacity,
-<<<<<<< HEAD
             String memo,
             LocalDateTime createdAt,
             LocalDateTime modifiedAt
@@ -36,10 +27,3 @@ public record PlaceDTO(
         return new PlaceDTO(placeType, placeName, address, phoneNumber, capacity, memo, createdAt, modifiedAt);
     }
 }
-=======
-            String memo
-    ) {
-     return new PlaceDTO(placeType, placeName, address, phoneNumber, capacity, memo);
-    }
-}
->>>>>>> #2-api
