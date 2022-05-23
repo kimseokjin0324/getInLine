481916e314027cf57e3ee2bd445c8aff5b780412
kimseokjin <kimseokjin0324@gmail.com>
2022-05-23 23:51:53 +0900

    Implement #2 - APIAuthController, dto, and its tests
    `APIAuthController` 테스트 작성 및 입출력 기본 설계
    어드민 회원가입, 로그인에 사용할 데이터 클래스 작성

--- src/main/java/com/example/getInLine/controller/api/APIAuthController.java
+++ src/main/java/com/example/getInLine/controller/api/APIAuthController.java
@@ -1,8 +1,9 @@
 package com.example.getInLine.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.getInLine.dto.APIDataResponse;
+import com.example.getInLine.dto.AdminRequest;
+import com.example.getInLine.dto.LoginRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
@@ -11,13 +12,14 @@ public class APIAuthController {
     //-이 return 값을 뷰 resolving 하는게 아니고
     //이 값을 그대로 return 해주는게 @RestController가 가진
     //@ResponseBody 의 특징이다.
-    @GetMapping("/sign-up")
-    public String signUp() {
-        return "done.";
-    }
-    @GetMapping("/login")
-    public String login() {
-        return "done.";
+
+    @PostMapping("/sign-up")
+    public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest) {
+        return APIDataResponse.empty();
     }
 
+    @PostMapping("/login")
+    public APIDataResponse<String> login(@RequestBody LoginRequest loginRequest) {
+        return APIDataResponse.empty();
+    }
 }
