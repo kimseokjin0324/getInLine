671a0d1544605ec8190e6227ab0e84dfab60bf8e
kimseokjin <kimseokjin0324@gmail.com>
2022-05-29 23:44:46 +0900

    Edit #6 - APIEcceptionHandlerTest

--- src/main/java/com/example/getInLine/error/APIExceptionHandler.java
+++ src/main/java/com/example/getInLine/error/APIExceptionHandler.java
@@ -21,19 +21,9 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     //- ConstraintViolationException 터졌을때
     @ExceptionHandler
-    public ResponseEntity<Object> general(ConstraintViolationException e, WebRequest request) {
-        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
-
-        );
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
     }
-
 
 
     //- GeneralException이 터졌을때
@@ -41,29 +31,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
         ErrorCode errorCode = e.getErrorCode();
         HttpStatus status = errorCode.isClientSideError() ? HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR;
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
 
-        );
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, status, request);
     }
 
     //- GeneralException이 아닌 일반 Exception을 처리하는 메소드
     @ExceptionHandler
-    public ResponseEntity<Object> exception(Exception e,WebRequest request) {
-        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return super.handleExceptionInternal(
-                e,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
-                HttpHeaders.EMPTY,
-                status,
-                request
-
-        );
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.INTERNAL_ERROR, HttpHeaders.EMPTY, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     @Override
@@ -75,11 +50,17 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
              WebRequest request) {
         ErrorCode errorCode = status.is4xxClientError() ?
                 ErrorCode.SPRING_BAD_REQUEST : ErrorCode.SPRING_INTERNAL_ERROR;
+        return handleExceptionInternal(ex, errorCode, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return super.handleExceptionInternal(
-                ex,
-                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
                 headers,
                 status,
-                request);
+                request
+        );
     }
+
 }
