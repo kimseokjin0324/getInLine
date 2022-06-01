2c53426002b1017b1c5cdb9a288ea7df2e93a7b7
kimseokjin <kimseokjin0324@gmail.com>
2022-06-01 15:48:43 +0900

    Implement #13 -eventRepositoryTest
    JPA Test진행

--- src/test/java/com/example/getInLine/repository/EventRepositoryTest.java
+++ src/test/java/com/example/getInLine/repository/EventRepositoryTest.java
@@ -1,11 +1,22 @@
 package com.example.getInLine.repository;
 
+import com.example.getInLine.constant.EventStatus;
+import com.example.getInLine.constant.PlaceType;
+import com.example.getInLine.domain.Event;
+import com.example.getInLine.domain.Place;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -23,13 +34,53 @@ class EventRepositoryTest {
 
     @DisplayName("")
     @Test
-    void test(){
+    void test() {
 
         //Given
+        Place place = createPlace();
+        Event event = createEvent(place);
+        testEntityManager.persist(place);
+        testEntityManager.persist(event);
 
         //When
-//        sut.finAl
+        Iterable<Event> events = sut.findAll(new BooleanBuilder());
         //Then
+        assertThat(events).hasSize(7);
     }
 
+    private Event createEvent(Place place) {
+        return createEvent(
+                place,
+                "test event",
+                EventStatus.ABORTED,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+    private Event createEvent(
+            Place place,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDateTime,
+            LocalDateTime eventEndDateTime
+    ) {
+        return Event.of(
+                place,
+                eventName,
+                eventStatus,
+                eventStartDateTime,
+                eventEndDateTime,
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        );
+
+    }
+
+    private Place createPlace() {
+        return Place.of(PlaceType.COMMON, "test place", "test address", "010-1234-1234", 10, null);
+
+
+    }
 }
